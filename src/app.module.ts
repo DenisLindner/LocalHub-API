f3df0313eae33493d552860fc705b659d83aea86
Denis Lindner <lindnerdenis19@gmail.com>
2026-06-19 13:25:40 -0300

    feat: create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './config/env.validation';
+import { UsersModule } from './modules/users/users.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { CostumersModule } from './modules/costumers/costumers.module';
+import { ProductsservicesModule } from './modules/productsservices/productsservices.module';
+import { MembersModule } from './modules/members/members.module';
 import databaseConfig from './config/database.config';
 import authConfig from './config/auth.config';
 import appConfig from './config/app.config';
@@ -20,6 +27,13 @@ import appConfig from './config/app.config';
     }),
     AuthModule,
     DatabaseModule,
+    UsersModule,
+    OrganizationsModule,
+    SubscriptionsModule,
+    TasksModule,
+    CostumersModule,
+    ProductsservicesModule,
+    MembersModule,
   ],
   controllers: [],
   providers: [],
